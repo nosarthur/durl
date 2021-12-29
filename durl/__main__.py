01bec632e4ef8d39f9bc586f150a5e9b55e97adb
Dong Zhou <zhou.dong@gmail.com>
2021-12-29 17:21:44 -0500

    add config parsing for repos.csv

--- durl/__main__.py
+++ durl/__main__.py
@@ -2,31 +2,26 @@ import click
 
 from . import util
 
-# repos.csv
-# repo1,/a/b/ct/,github
-# repo2,/a/b/ct/,opengrok
-
-
-prefix = 'https://github.com/nosarthur/'
-
-repos = {
-        'durl': '/Users/dzhou/src/durl',
-        }
 
 @click.command()
-@click.option('-r', '--repo', help='repo name')
+@click.option('-r', '--name', help='repo name')
 @click.option('-f', 'is_file_search', is_flag=True, help='search for files')
 @click.argument('keyword')
-def main(repo, is_file_search, keyword):
-    print(repo, is_file_search, keyword)
-
-    path = repos[repo]
+def main(name, is_file_search, keyword):
+    repo = util.get_repos()[name]
     if is_file_search:
-        got = util.find(path, keyword)
+        got = util.find(repo.path, keyword)
     else:
-        got = util.grep(path, keyword)
+        got = util.grep(repo.path, keyword)
+    if not got:
+        print('No match!')
+
     for line in got:
-        print(prefix + line)
+        if is_file_search:
+            print(util.format_output(repo.url, line))
+        else:
+            line, n = line.split(':')[:2]
+            print(util.format_output(repo.url, line, linenumber=n))
 
 
 if __name__ == '__main__':
--- durl/util.py
+++ durl/util.py
@@ -1,31 +1,62 @@
 import os
+import csv
 import subprocess
 from glob import glob
-from typing import List
+from pathlib import Path
+from typing import List, Dict
+from collections import namedtuple
 
-# https://github.com/nosarthur/durl/blob/
-#           main/LICENSE#L12
+# https://github.com/nosarthur/durl/blob/main/LICENSE#L12
 # TODO: site => sep
 sep = '#L'
+sep = '#'
+
+
+Repo = namedtuple('Repo', 'url path')
+
+
+def get_config_dir() -> Path:
+    root = os.environ.get('XDG_CONFIG_HOME') or os.path.join(
+        os.path.expanduser('~'), '.config')
+    return Path(root) / 'durl'
+
+
+def get_repos() -> Dict[str, Repo]:
+    """
+
+    """
+    repo_config = get_config_dir() / 'repos.csv'
+    if not repo_config.is_file() or repo_config.stat().st_size == 0:
+        return {}
+    with open(repo_config) as f:
+        rows = csv.DictReader(f, ['name', 'url', 'path'],
+                              restval='')  # it's actually a reader
+        repos = {r['name']: Repo(r['url'], r['path'])
+                for r in rows}
+    return repos
+
+
+
+def format_output(prefix, filename: str, linenumber: str=None) -> str:
+    """
+    """
+    if linenumber:
+        return f'{prefix}{filename}{sep}{linenumber}'
+    return prefix + filename
+
 
 
 def grep(path:str, keyword:str) -> List[str]:
     """
 
     """
-    p = subprocess.run(
-            f"grep -rin '{keyword}' *",
-            shell=True,
-            text=True,
-            capture_output=True,
-            )
+    cmd = f"grep -rinI --exclude-dir='.*' '{keyword}' *"
+    cmd = f"rg -Sn '{keyword}' "
+    # TODO: use string template to encode the command in a config file
+    p = subprocess.run(cmd, shell=True, text=True, capture_output=True,
+            cwd=path,)
     if p.stdout:
-        got = []
-        for l in p.stdout.split('\n'):
-            if l:
-                file, lno = l.split(':')[:2]
-                got.append(file + sep + lno)
-        return got
+        return [l for l in p.stdout.split('\n') if l]
     return []
 
 
@@ -34,4 +65,4 @@ def find(path:str, keyword:str) -> List[str]:
 
     """
     os.chdir(path)
-    return glob('**/' + keyword)
+    return glob('**/' + keyword, recursive=True)
